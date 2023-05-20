08448e689160022f619bcf32717681a2e2a72bc2
Laxmikantathare <laxmikantathare12@gmail.com>
2023-05-21 01:28:23 +0530

    success

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -50,7 +50,7 @@ export default function Contant() {
   
     <div className="container-fluid  d-flex justify-content-center mt-2 mb-3">
     <div className="container p-4 m-5 conbox "style={{ maxWidth:"37rem"}}>
-<form className='text-center' method='POST'>
+<form  autocomplete="off" className='text-center' method='POST'>
   <h4><strong>How Can I help?</strong></h4>
 
 <div className="form-group">
